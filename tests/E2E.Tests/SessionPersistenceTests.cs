7b46b146fbdf63baf64e24484f461af83b9fd379
Sam <Sam@SamGeek-tech>
2026-01-06 00:29:11 -0500

    me fixes

--- tests/E2E.Tests/SessionPersistenceTests.cs
+++ tests/E2E.Tests/SessionPersistenceTests.cs
@@ -12,7 +12,7 @@ public class SessionPersistenceTests : PageTest
     private string _baseUrl = "http://127.0.0.1:5002";
 
     [Test]
-    [Ignore("")]
+    [Ignore("Skipping scenario 10")]
     public async Task Scenario10_OrderPersistenceAcrossSessions()
     {
         // 1. Login and navigate to Orders
